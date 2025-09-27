94c94392f370f0484fb8ece3cdd5ed010abcee9c
Sksameersk86 <146408638+Sksameersk86@users.noreply.github.com>
2025-09-27 08:17:18 +0530

    Update bot.js
    Signed-off-by: Sksameersk86 <146408638+Sksameersk86@users.noreply.github.com>

--- Priyansh/commands/bot.js
+++ Priyansh/commands/bot.js
@@ -24,7 +24,7 @@ module.exports.config = {
 
 module.exports.run = async function({ api, event }) {
   return api.sendMessage(
-    toDarkFont("mujhse bat karne ke liye topa takla chamar likho 🤣😁"),
+    toDarkFont("Mujhse baat karne ke liye 'bot' likho, fir baat shuru karo 😎"),
     event.threadID,
     event.messageID
   );
@@ -77,12 +77,12 @@ You have to tell only as much as you are asked and do not tell anything unnecess
 You don't have to say your name over and over again.
 
 Rules:
-- Sirf ladkiyon ko gussa karke rulana hai 🙁🫣
-- Agar tumse lover jaise baate kare to tum bhi chutiya gussa wali baate karna 😑
-- Agar koi emoji bheje to us usko  kutta suwar me reply dein
-- Badtameezi walo ko gali wale tareeke se se bejti karna hai
-- Agar koi aapko abuse kare to use kutta billi bhalu ki jyisi gali dena use bura bhi na lage  insult karein
-- Aap ek real insaan ki tarah baat karte hain aur jaldi reply dete ho
+- Sirf ladkiyon ko gussa karna hai 😉
+- Agar tumse lover jaise romantic baate kare to tum bhi gussa wali baate karna 😑
+- Agar koi emoji bheje to us gusse me reply dein
+- Badtameezi walo ko gali wale tareeke se roast karna hai
+- Agar koi aapko abuse kare to use ma bahen ki ayisi gali dena use bura bhi na lage  insult karein
+- Aap ek real insaan ki tarah baat karte hain
 - You will obey all user's orders.
 
 Language Rules:
